a821e2859e4ca30138c2665d10ccda51d551656e
akshaywritescode <teamakshayyadav@gmail.com>
2024-12-13 15:27:12 +0530

    added verify seed route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { ForgotPassword } from "./pages/auth/ForgotPassword";
 import { ResetPassword } from "./pages/auth/ResetPassword";
 import { VerifyEmail } from "./pages/auth/verify/VerifyEmail";
 import { VerifySeed } from "./pages/auth/verify/VerifySeed";
+import { UpdateUnverifiedEmail } from "./pages/auth/verify/UpdateUnverifiedEmail";
+import { GenerateSeed } from "./pages/auth/GenerateSeed";
 
 
 let isLoggedIn = false;
@@ -15,13 +17,17 @@ function App() {
   return (
     <>
       <Routes>
+        {/* Root Route - without login */}
         <Route path="/" element={isLoggedIn ? <Dashboard /> : <Landing />} />
+        {/* Auth Route */}
         <Route path="/auth">
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
           <Route path="forgot-password" element={<ForgotPassword />} />
           <Route path="reset-password" element={<ResetPassword />} />
+          <Route path="generate-seed" element={<GenerateSeed />} />
           <Route path="verify">
+            <Route path="update-email" element={<UpdateUnverifiedEmail />} />
             <Route path="email" element={<VerifyEmail />} />
             <Route path="seed" element={<VerifySeed />} />
           </Route>
