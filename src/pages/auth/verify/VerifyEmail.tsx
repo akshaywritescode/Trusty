5419f7c8a83b6b5105a1961e25f36d9f9e069496
akshaywritescode <teamakshayyadav@gmail.com>
2024-12-13 15:28:16 +0530

    added verify email page

--- src/pages/auth/verify/VerifyEmail.tsx
+++ src/pages/auth/verify/VerifyEmail.tsx
@@ -1,7 +1,44 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { MailCheck } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
 export const VerifyEmail = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Verify Email</h1>
+    <div className="p-5 flex justify-center items-center w-screen h-screen">
+      <Card className="w-[450px]">
+        <CardHeader>
+          <CardTitle className="text-2xl">Verify Your Account</CardTitle>
+          <CardDescription className="flex flex-col gap-3">
+            <li>
+              We just sent an email to example@xyz.com with a verification link.
+              Click on the link to verify your account.
+            </li>
+            <li>You will be redirected to the next page automatically.</li>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <MailCheck className="w-20 h-20 mx-auto" />
+        </CardContent>
+        <CardFooter className="flex gap-3">
+          <Button className="w-full">Resend Mail</Button>
+          <Button
+            onClick={() => navigate("/auth/verify/update-email")}
+            className="w-full"
+          >
+            Update Mail
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
