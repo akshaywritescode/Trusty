146c166ca7e6a56c7584af8424b06cc7f1e5a9e4
akshaywritescode <teamakshayyadav@gmail.com>
2024-12-13 15:27:41 +0530

    added signup page

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -1,7 +1,48 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+
+import { OAuth } from "@/components/ui/oauth";
+import { PasswordInput } from "@/components/PasswordInput";
+
 export const Signup = () => {
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className="p-5 flex justify-center items-center w-screen h-screen">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle className="text-2xl">Create an account</CardTitle>
+          <CardDescription>
+            Enter your email & password below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" placeholder="xyz@example.com" />
+              </div>
+              <div className="flex flex-col space-y-1.5 relative">
+                <Label htmlFor="password">Password</Label>
+                <PasswordInput />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Create Account</Button>
+        </CardFooter>
+        <OAuth />
+      </Card>
     </div>
   );
 };
