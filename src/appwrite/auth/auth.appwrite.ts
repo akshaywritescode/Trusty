727bf2b5a41c98af9e027b2f678c063324ed9cf0
akshaywritescode <teamakshayyadav@gmail.com>
2024-12-14 11:11:47 +0530

    added login and email verification

--- src/appwrite/auth/auth.appwrite.ts
+++ src/appwrite/auth/auth.appwrite.ts
@@ -1,26 +1,35 @@
-import { Client, Account } from "appwrite";
-import { v4 as uuidv4 } from "uuid";
+import { Client, Account, ID } from "appwrite";
 import { TSignupResponseSchema } from "../../validation/signup.validate";
 
 const client = new Client()
+  .setEndpoint(import.meta.env.VITE_APPWRITE_API_ENDPOINT)
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 const account = new Account(client);
 
 export const authAppwrite = () => {
+  //create account
   const createAccount = (
     email: string,
     password: string
   ): Promise<TSignupResponseSchema> => {
-    const uuid = uuidv4();
+    const uuid = ID.unique();
     const promise = account.create(uuid, email, password);
     return promise;
   };
 
-  const verifyAccount = () => {
-    const promise = account.createVerification("https://example.com");
+  //login session
+  const createLoginSession = (email: string, password: string) => {
+    const promise = account.createEmailPasswordSession(email, password);
     return promise;
   };
 
-  return { createAccount, verifyAccount };
+  //create verification mail
+  const createVerificationMail = () => {
+    const verificationUrl = `http://localhost:5173/auth/verify/ensureVerification`;
+    const promise = account.createVerification(verificationUrl);
+    return promise;
+  };
+
+  return { createAccount, createLoginSession, createVerificationMail };
 };
